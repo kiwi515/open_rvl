eed90a0a6bcbb963511ebd7aee04410f00a24527
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-06-17 16:40:27 -0400

    OS/OSMessage

--- include/OS/OSThread.h
+++ include/OS/OSThread.h
@@ -6,10 +6,18 @@
 extern "C" {
 #endif
 
+#define OS_PRIORITY_MIN 0
+#define OS_PRIORITY_MAX 32
+
 typedef struct OSThread {
-    // TO-DO: Figure out how big OSContext is
     OSContext context;
-    char UNK_0x2C8[0x2FC - 0x2C8];
+    u16 state;    // at 0x2C8
+    s32 priority; // at 0x2D0
+    char UNK_0x2D4[0x2DC - 0x2D4];
+    struct OSThreadQueue* queue; // at 0x2DC
+    char UNK_0x2E0[0x2F0 - 0x2E0];
+    struct OSMutex* mutex; // at 0x2F0
+    char UNK_0x2F4[0x2FC - 0x2F4];
     struct OSThread* next; // at 0x2FC
     char UNK_0x300[0x4];
     void* stack_top; // at 0x304
@@ -17,12 +25,22 @@ typedef struct OSThread {
     char UNK_0x30C[0xC];
 } OSThread;
 
+typedef struct OSThreadQueue {
+    OSThread* THREAD_0x0;
+    OSThread* THREAD_0x4;
+} OSThreadQueue;
+
 OSThread* OS_FIRST_THREAD : 0x800000DC;
+
+void OSInitThreadQueue(OSThreadQueue*);
 
 u32 OSDisableScheduler(void);
 u32 OSEnableScheduler(void);
 
 void __OSReschedule(void);
+
+void OSSleepThread(OSThreadQueue*);
+void OSWakeupThread(OSThreadQueue*);
 
 #ifdef __cplusplus
 }
