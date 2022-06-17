3227b531d8dc0b3695e65793fb486491ae052bd8
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-06-17 14:26:55 -0400

    OS/OSCache

---
+++ include/DB/db.h
@@ -0,0 +1,13 @@
+#ifndef RVL_SDK_DB_H
+#define RVL_SDK_DB_H
+#include <types.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void DBPrintf(const char*, ...);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
--- include/PPC/PPCArch.h
+++ include/PPC/PPCArch.h
@@ -8,8 +8,17 @@ extern "C" {
 u32 PPCMfmsr(void);
 void PPCMtmsr(u32);
 
+u32 PPCMfhid0(void);
+void PPCMthid0(u32);
+
+u32 PPCMfl2cr(void);
+void PPCMtl2cr(u32);
+
 u32 PPCMffpscr(void);
 void PPCMtfpscr(u32);
+
+u32 PPCMfhid2(void);
+void PPCMthid2(u32);
 
 void PPCHalt(void);
 
