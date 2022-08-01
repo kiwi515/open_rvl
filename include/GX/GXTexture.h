2c3ceffcd829f998c73ec89da34a83eaec9e6f83
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-08-01 19:05:57 -0400

    TPL/TPL

--- include/GX/GXTexture.h
+++ include/GX/GXTexture.h
@@ -11,6 +11,37 @@ typedef enum _GXTexCoord {
     GX_TEX_COORD_0,
 } GXTexCoord;
 
+typedef enum _GXTexFilter { GX_LINEAR = 1, GX_LIN_MIP_LIN = 5 } GXTexFilter;
+
+typedef enum _GXTexFmt {
+    GX_TF_I4,
+    GX_TF_I8,
+    GX_TF_IA4,
+    GX_TF_3,
+    GX_TF_4,
+    GX_TF_5,
+    GX_TF_RGBA8,
+    GX_TF_7,
+
+    // CI formats
+    GX_TF_8,
+    GX_TF_9,
+    GX_TF_10,
+
+    GX_TF_11,
+    GX_TF_12,
+    GX_TF_13,
+    GX_TF_14,
+    GX_TF_15,
+    GX_TF_16,
+    GX_TF_17,
+    GX_TF_18,
+    GX_TF_19,
+    GX_TF_20,
+    GX_TF_21,
+    GX_TF_22
+} GXTexFmt;
+
 typedef enum _GXTexMapID {
     GX_TEXMAP_0,
     GX_TEXMAP_1,
@@ -23,6 +54,15 @@ typedef enum _GXTexMapID {
 
     GX_MAX_TEXMAP
 } GXTexMapID;
+
+typedef enum _GXTexWrapMode {
+    GX_TEXWRAPMODE_0,
+    GX_TEXWRAPMODE_1,
+    GX_TEXWRAPMODE_2,
+    GX_MAX_TEXWRAPMODE
+} GXTexWrapMode;
+
+typedef enum _GXTlutFmt { GX_TLUT_FMT_0 } GXTlutFmt;
 
 void __GXSetSUTexRegs(void);
 
---
+++ src/TPL/TPL.c
@@ -0,0 +1,44 @@
+#include "TPL.h"
+
+#include <OS/OSError.h>
+
+#define TPL_VERSION 0x0020AF30
+
+void TPLBind(TPLPalette* pal) {
+    u16 i;
+
+    if (pal->version != TPL_VERSION) {
+#line 25
+        OSError("invalid version number for texture palette");
+    }
+
+    pal->imageTable = (TPLDescriptor*)(pal->imageTableOfs + (u32)pal);
+
+    for (i = 0; i < pal->numImages; i++) {
+        if (pal->imageTable[i].imageHeader != NULL) {
+            pal->imageTable[i].imageHeader =
+                (TPLImageHeader*)((char*)pal + pal->imageTable[i].imageOfs);
+
+            if (!pal->imageTable[i].imageHeader->unpacked) {
+                pal->imageTable[i].imageHeader->data =
+                    (char*)pal + pal->imageTable[i].imageHeader->offset;
+                pal->imageTable[i].imageHeader->unpacked = TRUE;
+            }
+        }
+
+        if (pal->imageTable[i].palHeader != NULL) {
+            pal->imageTable[i].palHeader =
+                (TPLPaletteHeader*)((char*)pal + pal->imageTable[i].palOfs);
+
+            if (!pal->imageTable[i].palHeader->unpacked) {
+                pal->imageTable[i].palHeader->data =
+                    (char*)pal + pal->imageTable[i].palHeader->offset;
+                pal->imageTable[i].palHeader->unpacked = TRUE;
+            }
+        }
+    }
+}
+
+TPLDescriptor* TPLGet(TPLPalette* pal, u32 id) {
+    return &pal->imageTable[id % pal->numImages];
+}
