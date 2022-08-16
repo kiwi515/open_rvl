af974f843e3c3cfa9a6f14302918b956c3155ee2
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-08-16 18:12:19 -0400

    OS/OSUtf

---
+++ include/OS/OSUtf.h
@@ -0,0 +1,17 @@
+#ifndef RVL_SDK_OS_UTF_H
+#define RVL_SDK_OS_UTF_H
+#include "OS.h"
+#include <types.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const u8* OSUTF8to32(const u8*, u32*);
+const u16* OSUTF16to32(const u16*, u32*);
+u8 OSUTF32toANSI(u32);
+u16 OSUTF32toSJIS(u32);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
