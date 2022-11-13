d6de608a6e682f2dc71353b178b2207e970f3600
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-11-13 17:51:56 -0500

    fix_asm: Make all functions global

--- tools/tester/src/fix_asm.py
+++ tools/tester/src/fix_asm.py
@@ -1,19 +1,4 @@
 """Fixes GAS assembly to work with the tester.
-
-1. Prevent function-local labels from being emitted
-- lbl_* -> .L_*
-
-2. Correct SDA-macro relocations
-_SDA_BASE_ -> @sda21
-_SDA2_BASE_ -> @sda21
-
-3. Correct r13 relocations
-stw rX, Y@sda21(r13) -> stw rX, Y@sda21(0)
-lwz rX, Y@sda21(r13) -> lwz rX, Y@sda21(0)
-addi rX, r13, Y@sda21 -> li rX, Y@sda21
-
-These changes are only required because the objects are compared before linking,
-so these instruction patterns do actually generate different code at that stage.
 """
 
 from sys import argv
@@ -23,25 +8,30 @@ from re import match
 SECTION_REGEX = r"^\s*.section\s+(?P<Name>.[a-zA-Z0-9_$]+)"
 # Label declaration
 LABEL_REGEX = r"^\s*(?P<Name>lbl_[0-9A-F]{8})\s*:"
+# Function declaration
+FUNC_REGEX = r"(?P<Name>^\w+):"
 
 
 def fix(asm: list[str]) -> list[str]:
     """Fixup assembly file to work with the tester
     """
     # First pass:
-    # Find all function-local labels (.text)
+    # Find all function-local labels
     labels = []
     curr_sect = None
-    for line in asm:
+    for i, line in enumerate(asm):
         # Search line for patterns
         sect_match = match(SECTION_REGEX, line)
         lbl_match = match(LABEL_REGEX, line)
+
         # Are we looking at a section declaration?
         if sect_match != None:
             curr_sect = sect_match.group("Name")
-        # Are we looking at a function-local label declaration?
-        elif lbl_match != None and curr_sect == ".text":
-            labels.append(lbl_match.group("Name"))
+
+        if curr_sect == ".text":
+            # Are we looking at a function-local label declaration?
+            if lbl_match != None:
+                labels.append(lbl_match.group("Name"))
 
     # Second pass:
     # Correct relocations, and rename local labels
@@ -64,11 +54,16 @@ def fix(asm: list[str]) -> list[str]:
         # !!! Assumes doldisasm format !!!
         if line.startswith("/*"):
             insn = line[37:].split()
-
             # GAS addi -> li
             if insn[0] == "addi" and "@sda21" in line:
                 assert len(insn) == 4
                 line = line[:37] + f"li {insn[1]} {insn[3]}\n"
+        # Make functions global so their branch isn't resolved before linking
+        else:
+            func_match = match(FUNC_REGEX, line)
+            if func_match != None and not asm[i - 1].startswith(".global"):
+                fn_name = func_match.group("Name")
+                line = f".global {fn_name}; {fn_name}:"
 
         asm[i] = line
     return asm
