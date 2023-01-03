75659d70e63452f3f12ea6b43490d14e6b32f120
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-01-03 17:33:59 -0500

    VF/pf_w_clib

--- include/VF/pf_w_clib.h
+++ include/VF/pf_w_clib.h
@@ -6,6 +6,8 @@ extern "C" {
 #endif
 
 size_t VFipf_w_strlen(const wchar_t* str);
+wchar_t* VFipf_w_strcpy(wchar_t* dst, const wchar_t* src);
+int VFipf_w_strncmp(const wchar_t* s1, const wchar_t* s2, size_t n);
 
 #ifdef __cplusplus
 }
---
+++ src/VF/pf_w_clib.c
@@ -0,0 +1,37 @@
+#include "pf_w_clib.h"
+
+size_t VFipf_w_strlen(const wchar_t* str) {
+    const wchar_t* it = str;
+    ptrdiff_t diff;
+
+    // Find end of string
+    for (; *it != L'\0'; it++) {
+        ;
+    }
+
+    // Calculate size
+    diff = (uintptr_t)it - (uintptr_t)str;
+    return diff >> 1;
+}
+
+wchar_t* VFipf_w_strcpy(wchar_t* dst, const wchar_t* src) {
+    // Preserve original pointer
+    wchar_t* work_dst = dst;
+
+    for (; *work_dst = *src, *work_dst != L'\0'; src++, work_dst++) {
+        ;
+    }
+
+    return dst;
+}
+
+int VFipf_w_strncmp(const wchar_t* s1, const wchar_t* s2, size_t n) {
+
+    for (; n > 0; n--, s1++, s2++) {
+        if (*s1 == '\0' || *s2 == '\0' || *s1 != *s2) {
+            return *s1 - *s2;
+        }
+    }
+
+    return 0;
+}
