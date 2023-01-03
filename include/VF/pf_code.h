c2f5fc169b7ed740ce1975ba24179601ac9548ef
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-01-03 12:41:12 -0500

    Add pf_code rodata

--- include/VF/pf_code.h
+++ include/VF/pf_code.h
@@ -5,6 +5,17 @@
 extern "C" {
 #endif
 
+#define VF_FN_MIN_CHAR 0x20
+#define VF_FN_MAX_CHAR 0x80
+
+typedef enum {
+    VF_FN_CHAR_INVALID = 0,
+    VF_FN_CHAR_VALID_LONG = (1 << 0),
+    VF_FN_CHAR_VALID_SHORT = (1 << 1),
+} VFFnCharValidity;
+
+extern const u8 VFipf_valid_fn_char[VF_FN_MAX_CHAR - VF_FN_MIN_CHAR];
+
 int VFiPFCODE_Combine_Width(s16 lo, s16 hi);
 void VFiPFCODE_Divide_Width(u32 ch, s16* loOut, s16* hiOut);
 
