c6efcd88954272f0d85c4edce3b9e1b442c927c1
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-02-15 21:29:32 -0500

    Add `sinf`/`tanf` to STL

--- include/stl/internal/math_double.h
+++ include/stl/internal/math_double.h
@@ -1,6 +1,8 @@
 #ifndef MSL_MATH_DOUBLE_H
 #define MSL_MATH_DOUBLE_H
 #include "s_cos.h"
+#include "s_sin.h"
+#include "s_tan.h"
 #include "w_sqrt.h"
 #include <types.h>
 #ifdef __cplusplus
@@ -8,7 +10,9 @@ extern "C" {
 #endif
 
 static inline float sqrtf(float x) { return sqrt(x); }
+static inline float sinf(float x) { return sin(x); }
 static inline float cosf(float x) { return cos(x); }
+static inline float tanf(float x) { return tan(x); }
 
 float fabsf(float);
 
