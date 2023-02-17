a8d237446bef772604aa4db3bf7f6e99a8805b28
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-02-17 01:06:42 -0500

    Create headers for MTX library

--- include/MTX/mtx.h
+++ include/MTX/mtx.h
@@ -12,20 +12,30 @@ typedef struct Vec {
     f32 x, y, z;
 } Vec;
 
+typedef struct Quaternion {
+    f32 x, y, z, w;
+} Quaternion;
+
 void PSMTXIdentity(Mtx);
 void PSMTXCopy(const Mtx, Mtx);
+void PSMTXConcat(const Mtx, const Mtx, Mtx);
+void PSMTXConcatArray(const Mtx, const Mtx, Mtx, u32);
+void PSMTXTranspose(const Mtx, Mtx);
 void PSMTXInverse(const Mtx, Mtx);
 void PSMTXInvXpose(const Mtx, Mtx);
-void PSMTXConcat(const Mtx, const Mtx, Mtx);
-void PSMTXRotAxisRad(Mtx, const struct Vec*, f32);
+void PSMTXRotRad(Mtx, f32, char);
+void PSMTXRotTrig(Mtx, f32, f32, char);
+void PSMTXRotAxisRad(Mtx, const Vec*, f32);
+void PSMTXTrans(Mtx, f32, f32, f32);
 void PSMTXTransApply(const Mtx, Mtx, f32, f32, f32);
 void PSMTXScale(Mtx, f32, f32, f32);
 void PSMTXScaleApply(const Mtx, Mtx, f32, f32, f32);
+void PSMTXQuat(Mtx, const Quaternion*);
 
-void C_MTXLookAt(Mtx, Vec*, Vec*, Vec*);
-void C_MTXLightOrtho(Mtx, f32, f32, f32, f32, f32, f32, f32, f32);
+void C_MTXLookAt(Mtx, const Vec*, const Vec*, const Vec*);
 void C_MTXLightFrustum(Mtx, f32, f32, f32, f32, f32, f32, f32, f32, f32);
 void C_MTXLightPerspective(Mtx, f32, f32, f32, f32, f32, f32);
+void C_MTXLightOrtho(Mtx, f32, f32, f32, f32, f32, f32, f32, f32);
 
 #ifdef __cplusplus
 }
