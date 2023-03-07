7468b6ec18398ab5946d7a901ee8a8fa66c2a8f4
tmschiff <49212064+kiwi515@users.noreply.github.com>
2023-03-07 11:59:11 -0500

    Change `OSInitFastCast` to use inline asm

--- include/OS/OSFastCast.h
+++ include/OS/OSFastCast.h
@@ -5,23 +5,25 @@
 extern "C" {
 #endif
 
-static asm void OSInitFastCast(void) {
+static void OSInitFastCast(void) {
     // clang-format off
-    li r3, 4
-    oris r3, r3, 4
-    mtspr 0x392, r3
+    asm {
+        li r3, 4
+        oris r3, r3, 4
+        mtspr 0x392, r3
 
-    li r3, 5
-    oris r3, r3, 5
-    mtspr 0x393, r3
-    
-    li r3, 6
-    oris r3, r3, 6
-    mtspr 0x394, r3
-    
-    li r3, 7
-    oris r3, r3, 7
-    mtspr 0x395, r3
+        li r3, 5
+        oris r3, r3, 5
+        mtspr 0x393, r3
+        
+        li r3, 6
+        oris r3, r3, 6
+        mtspr 0x394, r3
+        
+        li r3, 7
+        oris r3, r3, 7
+        mtspr 0x395, r3
+    }
     // clang-format on
 }
 
